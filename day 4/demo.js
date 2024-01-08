979174f6ad4ca57c791d5b6ebae4c29b9aa0925c
Himanshusinghaiml <himanshusingh945443@gmail.com>
2024-01-08 18:19:25 +0530

    learning-dom in js

---
+++ day 4/demo.js
@@ -0,0 +1,12 @@
+
+
+ 
+function callmyfunction(){
+    document.getElementById('tikku').innerHTML="no , i think u r preparing for big tech companies ";
+}
+ 
+function madebyhimanshu(){
+
+    document.getElementById("possible").innerHTML="yes it may be ";
+}    
+ 
